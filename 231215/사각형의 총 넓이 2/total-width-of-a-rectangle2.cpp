7d2843fbc0948dd1a3f7e90c7cc53e5fe4f80f9f
ONELAE <87406659+onenewarm@users.noreply.github.com>
2023-12-15 11:27:23 +0900

    Title: 사각형의 총 넓이 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231215/사각형의 총 넓이 2/total-width-of-a-rectangle2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#define OFFSET 100
+
+using namespace std;
+
+int main() {
+    
+    int matrix[OFFSET*2 + 1][OFFSET*2 + 1] = {{0, }, };
+
+    int n;
+    cin >> n;
+    for(int iCnt = 0; iCnt < n;++iCnt)
+    {
+        int x1,y1,x2,y2;
+        cin >> x1 >> y1 >> x2 >> y2;
+
+        for(int iRow = y1+1; iRow <= y2;++iRow)
+        {
+            for(int iCol = x1+1;iCol <=x2;++iCol)
+            {
+                matrix[iRow][iCol] = 1;
+            }
+        }
+    }
+
+    int ans = 0;
+
+    for(int iRow = 0 ; iRow <= 200 ; ++iRow)
+    {
+        for(int iCol = 0; iCol <= 200 ; ++iCol)
+        {
+            if(matrix[iRow][iCol]) ++ans;
+        }
+    }
+
+    cout << ans;
+    return 0;
+}
