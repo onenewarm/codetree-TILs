a1319235808e6c3775b6b320c4541a9e0d305ad5
ONELAE <87406659+onenewarm@users.noreply.github.com>
2023-12-25 15:29:54 +0900

    Title: 최고의 13위치, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231225/최고의 13위치/best-place-of-13.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int N;
+    cin >> N;
+    int coinsFlag[20][20];
+
+    for(int iRow = 0 ; iRow < N ; ++iRow)
+    {
+        for(int iCol = 0 ; iCol < N ; ++iCol)
+        {
+            cin >> coinsFlag[iRow][iCol];
+        }
+    }
+
+    int ans = 0;
+
+    for(int iRow = 0 ; iRow < N ; ++iRow)
+    {
+        int curCoins = 0;
+        for(int iCol = 0; iCol < N - 2 ; ++iCol)
+        {
+            curCoins += coinsFlag[iRow][iCol] + coinsFlag[iRow][iCol+1] + coinsFlag[iRow][iCol+2];
+        }
+        ans = max(ans, curCoins);
+    }
+
+    cout << ans;
+
+    return 0;
+}
