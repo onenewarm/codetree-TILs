9b48862889be23c5cd8033c758f01459a7e710b6
ONELAE <87406659+onenewarm@users.noreply.github.com>
2024-01-05 16:07:06 +0900

    Title: Carry 피하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240105/Carry 피하기 2/escaping-carry-2.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n;
+
+    cin >> n;
+
+    int nums[21];
+
+    for(int iCnt = 0; iCnt < n;++iCnt)
+    {
+        cin >> nums[iCnt];
+    }
+
+    int ans = 0;
+
+    for(int iFirst = 0; iFirst < n - 2;++iFirst)
+    {
+        for(int iSecond = iFirst+1;iSecond < n - 1; ++iSecond)
+        {
+            for(int iThird = iSecond+1;iThird < n ; ++iThird)
+            {
+                int modVal = 10;
+                
+                int First = nums[iFirst];
+                int Second = nums[iSecond];
+                int Third = nums[iThird];
+
+                bool isPossible = true;
+
+                while(First | Second | Third)
+                {
+                    int tmpSum = (First % modVal) + (Second % modVal) + (Third % modVal);
+
+                    if(tmpSum >= 10){
+                        isPossible = false;
+                        break;
+                    }
+
+                    First /= 10;
+                    Second /= 10;
+                    Third /= 10;
+                }
+
+                if(isPossible) ans = max(ans, nums[iFirst] + nums[iSecond] + nums[iThird]);
+            }
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
