785ba853847fef8df5fe753aeb9c8af5223047a6
ONELAE <87406659+onenewarm@users.noreply.github.com>
2024-01-08 15:15:05 +0900

    Title: 바구니 안의 사탕 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240108/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
+++ 240108/바구니 안의 사탕 2/candy-in-the-basket-2.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-int road[101];
+int road[1001];
 
 int main() {
     // 여기에 코드를 작성해주세요.
@@ -21,7 +21,7 @@ int main() {
     int ans = 0;
 
 // 0 1 2 3 4
-    for(int iStart = 0; iStart <= 100 - (2*k) ;++iStart)
+    for(int iStart = 0; iStart <= 1000 - (2*k) ;++iStart)
     {
         int sum = 0;
         for(int iCnt = 0; iCnt < (2*k+1);++iCnt)
