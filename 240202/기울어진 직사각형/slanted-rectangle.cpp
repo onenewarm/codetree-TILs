da4b2f0356bdfe9817265037be071e4d45ebd940
ONELAE <87406659+onenewarm@users.noreply.github.com>
2024-02-02 13:51:10 +0900

    Title: 기울어진 직사각형, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240202/기울어진 직사각형/slanted-rectangle.cpp
+++ 240202/기울어진 직사각형/slanted-rectangle.cpp
@@ -2,19 +2,19 @@
 
 using namespace std;
 
-int dy[4] = {-1, -1, 1, 1};
-int dx[4] = {1, -1, -1, 1};
+int dy[4] = { -1, -1, 1, 1 };
+int dx[4] = { 1, -1, -1, 1 };
 
 int main() {
     // 여기에 코드를 작성해주세요.
-    int n ;
+    int n;
     cin >> n;
 
     int matrix[21][21];
 
-    for(int iRow = 0 ; iRow < n ; ++iRow)
+    for (int iRow = 0; iRow < n; ++iRow)
     {
-        for(int iCol = 0 ; iCol < n ;++iCol)
+        for (int iCol = 0; iCol < n; ++iCol)
         {
             cin >> matrix[iRow][iCol];
         }
@@ -22,61 +22,61 @@ int main() {
 
     int ans = 0;
 
-    for(int iRow = 0; iRow < n ;++iRow)
+    for (int iRow = 0; iRow < n; ++iRow)
     {
-        for(int iCol = 0 ; iCol < n ; ++iCol)
+        for (int iCol = 0; iCol < n; ++iCol)
         {
-            for(int sideLen = 1 ; sideLen < n ; ++sideLen)
+            for (int sideLen = 1; sideLen < n - 1; ++sideLen)
             {
-                int nextY = iRow + (sideLen * dy[0]);
-                int nextX = iCol + (sideLen * dx[0]);
+                int firstNextY = iRow + (sideLen * dy[0]);
+                int firstNextX = iCol + (sideLen * dx[0]);
 
-                if(nextY < 0 || nextY > n - 1 || nextX < 0 || nextX > n - 1) continue;
+                if (firstNextY < 0 || firstNextY > n - 1 || firstNextX < 0 || firstNextX > n - 1) break;
 
-                for(int bottomLen = 1 ; bottomLen < n ; ++bottomLen)
+                for (int bottomLen = 1; bottomLen < n - sideLen; ++bottomLen)
                 {
-                    int sum = 0 ;
+                    int sum = 0;
 
-                    nextY = nextY + (bottomLen * dy[1]);
-                    nextX = nextX + (bottomLen * dx[1]);
+                    int nextY = firstNextY + (bottomLen * dy[1]);
+                    int nextX = firstNextX + (bottomLen * dx[1]);
 
-                    if(nextY < 0 || nextY > n - 1 || nextX < 0 || nextX > n - 1) continue;
+                    if (nextY < 0 || nextY > n - 1 || nextX < 0 || nextX > n - 1) break;
 
                     nextY = nextY + (sideLen * dy[2]);
                     nextX = nextX + (sideLen * dx[2]);
 
-                    if(nextY < 0 || nextY > n - 1 || nextX < 0 || nextX > n - 1) continue;
+                    if (nextY < 0 || nextY > n - 1 || nextX < 0 || nextX > n - 1) break;
 
                     nextY = nextY + (bottomLen * dy[3]);
                     nextX = nextX + (bottomLen * dx[3]);
 
-                    if(nextY < 0 || nextY > n - 1 || nextX < 0 || nextX > n - 1) continue;
+                    if (nextY < 0 || nextY > n - 1 || nextX < 0 || nextX > n - 1) break;
 
                     int curY = iRow;
                     int curX = iCol;
 
-                    for(int iFirst = 0 ; iFirst < sideLen ; ++iFirst)
+                    for (int iFirst = 0; iFirst < sideLen; ++iFirst)
                     {
                         curY = curY += dy[0];
                         curX = curX += dx[0];
                         sum += matrix[curY][curX];
                     }
 
-                    for(int iSecond = 0 ; iSecond < bottomLen ; ++iSecond)
+                    for (int iSecond = 0; iSecond < bottomLen; ++iSecond)
                     {
                         curY = curY += dy[1];
                         curX = curX += dx[1];
                         sum += matrix[curY][curX];
                     }
-                    
-                    for(int iThird = 0 ; iThird < sideLen ; ++iThird)
+
+                    for (int iThird = 0; iThird < sideLen; ++iThird)
                     {
                         curY = curY += dy[2];
                         curX = curX += dx[2];
                         sum += matrix[curY][curX];
                     }
 
-                    for(int iForth = 0 ; iForth < bottomLen ; ++iForth)
+                    for (int iForth = 0; iForth < bottomLen; ++iForth)
                     {
                         curY = curY += dy[3];
                         curX = curX += dx[3];
@@ -89,7 +89,7 @@ int main() {
         }
     }
 
-    
+
     cout << ans;
 
     return 0;
