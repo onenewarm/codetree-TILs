52ddfe987cb5dfc7c88754f70e13967d7a8d8a3b
ONELAE <87406659+onenewarm@users.noreply.github.com>
2024-02-14 13:16:50 +0900

    Title: 최단 Run Length 인코딩, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240214/최단 Run Length 인코딩/shortest-run-length-encoding.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+
+    int n = 0;
+    char A[11];
+
+    cin >> A;
+    char* p = A;
+    while(*p != '\0')
+    {
+        ++n;
+        ++p;
+    }
+
+    int res = 2147483647;
+
+    for(int startIdx = 0; startIdx < n ; ++startIdx)
+    {
+        char B[11];
+        for(int iCnt = 0; iCnt < n ; ++iCnt)
+        {
+            B[iCnt] = A[(startIdx + iCnt) % n];
+        }
+
+        int strLen = 2;
+        char prevCh = B[0];
+        for(int iCnt = 0 ;iCnt < n ;++iCnt)
+        {
+            if(prevCh != B[iCnt])
+            {
+                strLen += 2;
+                prevCh = B[iCnt];
+            }
+        }
+
+        res = min(res, strLen);
+    }
+
+    cout << res;
+
+    return 0;
+}
+
+/*
+고급요구사항 :
+길이가 n인 문자열 A가 있다.
+특정 횟수만큼 오른쪽으로 shift한다.
+
+*/
