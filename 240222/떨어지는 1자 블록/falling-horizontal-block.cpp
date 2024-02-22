54457b616cc9d98fb45e486d500e25c5605b6a25
ONELAE <87406659+onenewarm@users.noreply.github.com>
2024-02-22 11:42:11 +0900

    Title: 떨어지는 1자 블록, Time: 2ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240222/떨어지는 1자 블록/falling-horizontal-block.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+
+using namespace std;
+
+int n,m,k;
+int matrix[101][101];
+int highest[101];
+
+const int maxSize = 2147483647;
+
+void Solve()
+{
+    int collisionRow = maxSize;
+    for(int iCol = k ; iCol < k + m ; ++iCol)
+    {
+        if(collisionRow != 0) collisionRow = min(collisionRow, highest[iCol]);
+    }
+
+    int resCol;
+
+    if(collisionRow == maxSize) resCol = 1;
+    else resCol = collisionRow - 1;
+
+    for(int iCol = k ; iCol < k + m ;++iCol)
+    {
+        matrix[resCol][iCol] = 1;
+    }
+
+    for(int iRow = 1 ; iRow <=n ; ++iRow)
+    {
+        for(int iCol = 1 ; iCol <=n ;++iCol)
+        {
+            cout << matrix[iRow][iCol] << ' ';
+        }
+        cout << '\n';
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m >> k;
+
+    for(int iRow = 1 ; iRow <= n ; ++iRow)
+    {
+        for(int iCol = 1 ; iCol <= n ; ++iCol)
+        {
+            cin >> matrix[iRow][iCol];
+        }
+    }
+
+    for(int iCol = 1 ; iCol <= n ; ++iCol)
+    {
+        for(int iRow = 1 ; iRow <= n ; ++iRow)
+        {
+            if(matrix[iRow][iCol])
+            {
+                highest[iCol] = iRow;
+                break;
+            }
+        }
+    }
+
+    Solve();
+
+
+    return 0;
+}
