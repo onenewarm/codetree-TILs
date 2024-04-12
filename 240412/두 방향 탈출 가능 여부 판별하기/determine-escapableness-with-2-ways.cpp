621d3639f6fadde329e60d68e2ffbe40f2b2dd3f
ONELAE <87406659+onenewarm@users.noreply.github.com>
2024-04-12 22:22:44 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240412/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n,m;
+vector<vector<int>> matrix;
+
+int dy[2] = {0, 1};
+int dx[2] = {-1, 0};
+
+bool possibleFlag = false;
+
+void DFS(int curRow, int curCol)
+{
+    if(curRow == n -1 && curCol == m - 1)
+    {
+        possibleFlag = true;
+        cout << 1;
+        return;
+    }
+
+    for(int DCnt = 0 ; DCnt < 2 ; ++DCnt)
+    {
+        int nextY = curRow + dy[DCnt];
+        int nextX = curCol + dx[DCnt];
+
+        if(nextY < 0 || nextX < 0 || nextY > n -1 
+        || nextX > n - 1 || matrix[nextY][nextX] == 0) continue;
+
+        DFS(nextY, nextX);
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m;
+
+    matrix.resize(n, vector<int>(m));
+
+    for(int iRow = 0 ; iRow < n ; ++iRow)
+    {
+        for(int iCol = 0 ; iCol < m ; ++iCol)
+        {
+            cin >> matrix[iRow][iCol];
+        }
+    }
+
+    if(possibleFlag == false) cout << 0;
+
+    return 0;
+}
