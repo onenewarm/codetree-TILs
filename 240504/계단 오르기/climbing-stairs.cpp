d7f399f7ac10f6e1303e6613985c7f9088220e9a
ONELAE <87406659+onenewarm@users.noreply.github.com>
2024-05-04 21:47:53 +0900

    Title: 계단 오르기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240504/계단 오르기/climbing-stairs.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int n;
+//위치에 따른 가능한 경우의 수를 memo한다.
+int memo[1001];
+
+int DFS(int cur)
+{
+    if(memo[cur] != -1)
+    {
+        return memo[cur];
+    }
+
+    if(cur > n) return 0;
+    else if(cur == n) return memo[cur] = 1;
+    else if(cur == n - 1) return memo[cur] = 0;
+
+    memo[cur] = 0;
+
+    return memo[cur] += (DFS(cur + 2) + DFS(cur + 3));
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+
+    ::memset(memo, -1, 1001);
+    
+    DFS(0);
+
+    cout << memo[0];
+
+    return 0;
+}
+
+//2와 3의 더하기 조합으로 n만들기
