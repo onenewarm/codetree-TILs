88803a0cb16e7675210048ac00619ed1b07d2ca7
ONELAE <87406659+onenewarm@users.noreply.github.com>
2024-08-01 13:18:00 +0900

    Title: 편집거리, Time: 2ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240801/편집거리/edit-distance.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+
+string A,B;
+
+int DFS(int i, int j)
+{
+    if(i < 0 || j < 0)  return 1;
+    
+    if(A[i] == B[j])
+    {
+        return DFS(i-1, j-1);
+    }
+    else
+    {
+        int minVal = min(DFS(i-1, j), DFS(i,j-1));
+        minVal = min(minVal, DFS(i-1, j-1));
+        return minVal + 1;
+    }
+}
+
+
+int main() {
+    // 여기에 코드를 작성해주세요
+    cin >> A >> B;
+    
+    cout << DFS(A.size() - 1, B.size() - 1);
+    
+    return 0;
+}
