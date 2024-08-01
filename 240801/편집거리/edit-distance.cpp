1f9edc0c61132318392c3827156254044c638ae7
ONELAE <87406659+onenewarm@users.noreply.github.com>
2024-08-01 13:21:16 +0900

    Title: 편집거리, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240801/편집거리/edit-distance.cpp
+++ 240801/편집거리/edit-distance.cpp
@@ -6,20 +6,24 @@ using namespace std;
 
 string A,B;
 
+int dp[1001][1001];
+
 int DFS(int i, int j)
 {
+    if(dp[i][j] != -1) return dp[i][j];
+
     if(i < 0 && j < 0) return 0;
     else if(i < 0 || j < 0) return 1;
     
     if(A[i] == B[j])
     {
-        return DFS(i-1, j-1);
+        return dp[i][j] = DFS(i-1, j-1);
     }
     else
     {
         int minVal = min(DFS(i-1, j), DFS(i,j-1));
         minVal = min(minVal, DFS(i-1, j-1));
-        return minVal + 1;
+        return dp[i][j] = minVal + 1;
     }
 }
 
@@ -28,6 +32,14 @@ int main() {
     // 여기에 코드를 작성해주세요
     cin >> A >> B;
     
+    for(int i = 0 ; i < A.size() ; ++i)
+    {
+        for(int j = 0 ; j < B.size() ; ++j)
+        {
+            dp[i][j] = -1;
+        }
+    }
+
     cout << DFS(A.size() - 1, B.size() - 1);
     
     return 0;
