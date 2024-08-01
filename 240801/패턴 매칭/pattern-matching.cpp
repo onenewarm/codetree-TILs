7e62738aff684b238f13d5267684406497e7f243
ONELAE <87406659+onenewarm@users.noreply.github.com>
2024-08-01 14:37:55 +0900

    Title: 패턴 매칭, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240801/패턴 매칭/pattern-matching.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string s,p;
+
+bool DFS(int i, int j, bool starFlag)
+{
+    if(i < 0 || j < 0)
+    {
+        if(i - j) return false;
+        else return true;
+    }
+
+    if(s[i] == p[j]) return DFS(i-1, j-1, false);
+    else
+    {
+        if(p[j] == '.') return DFS(i-1, j-1, false);
+        
+        if(p[j] == '*') return DFS(i, j-1, true);
+
+        if(starFlag == false) return false;
+        else return DFS(i, j-1, false);
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요
+
+    cin >> s >> p;
+    
+    bool res = DFS(s.size() - 1, p.size() - 1, false);
+
+    if(res == true) cout << "true";
+    else cout << "false";
+
+    return 0;
+}
