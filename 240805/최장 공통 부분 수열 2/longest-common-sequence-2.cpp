0d1997ee2cdcc135d45e7c7caf61824649989676
ONELAE <87406659+onenewarm@users.noreply.github.com>
2024-08-05 14:13:42 +0900

    Title: 최장 공통 부분 수열 2, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240805/최장 공통 부분 수열 2/longest-common-sequence-2.cpp
+++ 240805/최장 공통 부분 수열 2/longest-common-sequence-2.cpp
@@ -34,6 +34,8 @@ int main() {
     int j = B.size();
     int prevDP = dp[i][j];
 
+    if(A[i-1] == B[j-1]) res = A[i-1];
+
 /*
     for(int Row = 1 ; Row <= A.size() ; ++Row)
     {
@@ -43,20 +45,26 @@ int main() {
         }
         cout << endl;
     }
-*/
+    */
+
     while(1)
     {
         if(i < 1 || j < 1)
         {
+            //cout << "!";
             if(i < 1)
             {
+                //cout << "@";
                 for(int curj = j ; curj > 0 ; --curj)
                 {
+                    //cout << "#";
+                    //cout << prevDP;
                     if(dp[i+1][curj] == prevDP - 1)
                     {
+                        //cout << "%";
                         prevDP = dp[i+1][curj];
-                        //printf("Add : %d, %d, %c\n", i, curj, B[curj]);
-                        res = B[curj] + res;
+                        //printf("Add : %d, %d, %c\n", i, curj, B[curj-1]);
+                        res = B[curj-1] + res;
                     }
                 }
             }
@@ -67,8 +75,8 @@ int main() {
                    if(dp[curi][j+1] == prevDP - 1)
                     {
                         prevDP = dp[curi][j+1];
-                        //printf("Add : %d, %d, %c\n", curi, j, A[curi]);
-                        res = A[curi] + res;
+                        //printf("Add : %d, %d, %c\n", curi, j, A[curi-1]);
+                        res = A[curi-1] + res;
                     } 
                 }
             }
@@ -79,15 +87,16 @@ int main() {
         {
             if(dp[i-1][j] == prevDP - 1)
             {
-                //printf("Add : %d, %d, %c\n", i, j, B[j]);
-                res = B[j] + res;
+                //printf("Add : %d, %d, %c\n", i, j, B[j-1]);
+                res = B[j-1] + res;
             }
             else
             {
-                //printf("Add : %d, %d, %c\n", i, j, A[i]);
-                res = A[i] + res;
+                //printf("Add : %d, %d, %c\n", i, j, A[i-1]);
+                res = A[i-1] + res;
             }
             prevDP = dp[i][j];
+            //cout <<"***"<< i << j << prevDP;
         }
         i--;
         j--;
